8c6669fc9af8ad18239b8b0d3a92b6ba903888f2
Emmanuel Kant <Emmanuel Kant@EmmanuelKant>
2016-10-22 18:22:25 -0200

    Criação do Método final e elaboração do mesmo

--- Raciocionio/src/Funcoes.java
+++ Raciocionio/src/Funcoes.java
@@ -6,23 +6,23 @@ public class Funcoes {
 
 		Scanner in = new Scanner(System.in);
 		int valor = 0;
-		do{
-		if (controler == 0) {
-			System.out.print("Digite o valor de 'P':");
-			valor = in.nextInt();
-		}
-		if (controler == 1) {
-			System.out.print("Digite o valor de 'Q':");
-			valor = in.nextInt();
-		}
-		if (controler == 2) {
-			System.out.print("Digite o valor de 'R':");
-			valor = in.nextInt();
-		}
-		if(valor>1 || valor<0){
-			System.out.println("\nERROR: Por favor digite o numeor '1' para VERDADEIRO e '0' para FALSO.\n");
-		}
-		}while(valor>1 || valor<0);
+		do {
+			if (controler == 0) {
+				System.out.print("Digite o valor de 'P': ");
+				valor = in.nextInt();
+			}
+			if (controler == 1) {
+				System.out.print("Digite o valor de 'Q': ");
+				valor = in.nextInt();
+			}
+			if (controler == 2) {
+				System.out.print("Digite o valor de 'R': ");
+				valor = in.nextInt();
+			}
+			if (valor > 1 || valor < 0) {
+				System.out.println("\nERROR: Por favor digite o numeor '1' para VERDADEIRO e '0' para FALSO.\n");
+			}
+		} while (valor > 1 || valor < 0);
 		if (valor == 1) {
 			return true;
 		} else {
@@ -32,24 +32,55 @@ public class Funcoes {
 
 	public static void intro() {
 		Scanner in = new Scanner(System.in);
-		System.out.println("\nEsse programa irÃ¡ resolver este problema --> Pv(Q^R)<->(PvQ)^(PvR)");
-		System.out.println("Mas antes digite os valores das preposiÃ§Ãµes P, Q e R.");
-		System.out.println("\n\n!!!!!!AtenÃ§Ã£o!!!!!!\n\n");
+		System.out.println("\nEsse programa irá¡ resolver este problema --> Pv(Q^R)<->(PvQ)^(PvR)");
+		System.out.println("Mas antes digite os valores das preposições P, Q e R.");
+		System.out.println("\n\n!!!!!!Atenção!!!!!!\n\n");
 		System.out.println("Para selecionar o valor VERDADEIRO: Digite o numero '1' e aperte [ENTER]");
 		System.out.println("Para selecionar o valor FALSO: Digite o numero '0' e aperte [ENTER]");
 		System.out.print("Aperte [ENTER] para continuar\n");
 		String start = in.nextLine();
 	}
-	
+
 	public static int mostraValores(boolean p, boolean q, boolean r) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("\nAs suas escolhas ficaram desta maneira:");
-		System.out.println("\nA preposiÃ§Ã£o 'P' ficou com o valor:"+p);
-		System.out.println("\nA preposiÃ§Ã£o 'Q' ficou com o valor:"+q);
-		System.out.println("\nA preposiÃ§Ã£o 'R' ficou com o valor:"+r);
-		System.out.println("\nVocÃª Confirma suas escolhas? [Digite '1' para SIM e '0' para NÃƒO]");
+		System.out.println("\nA preposição 'P' ficou com o valor: " + p);
+		System.out.println("\nA preposição 'Q' ficou com o valor: " + q);
+		System.out.println("\nA preposição 'R' ficou com o valor: " + r);
+		System.out.println("\nVocê Confirma suas escolhas? [Digite '1' para SIM e '0' para NÃO]");
 		int decision = in.nextInt();
 		return decision;
+	}
+
+	public static void tabela(boolean p, boolean q, boolean r, boolean qANDr, boolean pORq, boolean pORr, boolean p_OR_qANDr, boolean pORq_AND_pORr, boolean equacaoFinal) {
+		Scanner in = new Scanner(System.in);
+		//Essa variavel 'prep' que estou criando é o nome reduzido de preposição, irei criar um array de 9 posições.
+		char[] prep  = new char[9];
+		prep[0] = Funcoes.conversor(p);
+		prep[1] = Funcoes.conversor(q);
+		prep[2] = Funcoes.conversor(r);
+		prep[3] = Funcoes.conversor(qANDr);
+		prep[4] = Funcoes.conversor(pORq);
+		prep[5] = Funcoes.conversor(pORr);
+		prep[6] = Funcoes.conversor(p_OR_qANDr);
+		prep[7] = Funcoes.conversor(pORq_AND_pORr);
+		prep[8] = Funcoes.conversor(equacaoFinal);
+		System.out.println("|_P_|_Q_|_R_|_(Q^R)_|_(PvQ)_|_(PvR)_|_Pv(Q^R)_|_(PvQ)^(PvR)_|_Pv(Q^R)<->(PvQ)^(PvR)_|");
+		System.out.println("|_"+prep[0]+"_|_"+prep[1]+"_|_"+prep[2]+"_|___"+prep[3]+"___|___"+prep[4]+"__|___"+prep[5]+"__|_____"+prep[6]+"___|______"+prep[7]+"____|___________"+prep[8]+"___________|\n\n");
+		System.out.print("APERTE [ENTER] PARA FINALIZAR A APLICAÇÃO");
+		String finalizacao = in.nextLine();
+		System.out.println("-------------------");
+		System.out.println("        ----------");
+		System.out.println("            -----");
+		System.out.println("APLICAÇÃO FINALIZADA");
+	}
+	
+	private static char conversor(boolean x){
+		if (x == true){
+			return 86;
+		}else {
+			return 70;
+		}
 	}
 
 }
--- Raciocionio/src/Main.java
+++ Raciocionio/src/Main.java
@@ -3,28 +3,30 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner (System.in);
+		Scanner in = new Scanner(System.in);
 		int controler = 0;
 		boolean p = true, q = true, r = true;
-		
-		
+
 		Funcoes.intro();
 		int decision = 0;
-		do{
-		p = Funcoes.pegaValor(controler);
-		controler += 1;
-		q = Funcoes.pegaValor(controler);
-		controler += 1;
-		r = Funcoes.pegaValor(controler);
-		decision = Funcoes.mostraValores(p,q,r);
-		}while (decision == 1);
-		boolean qANDr = Equacoes.first(q,r);
-		boolean pORq = Equacoes.second(p,q);
-		boolean pORr = Equacoes.third(p,r);
-		boolean p_OR_qANDr = Equacoes.fourth(p,qANDr);
+		do {
+			p = Funcoes.pegaValor(controler);
+			controler += 1;
+			q = Funcoes.pegaValor(controler);
+			controler += 1;
+			r = Funcoes.pegaValor(controler);
+			decision = Funcoes.mostraValores(p, q, r);
+			if (decision == 0) {
+				System.out.println("\nEntão digite os valores novamente por favor");
+			}
+		} while (decision == 0);
+		boolean qANDr = Equacoes.first(q, r);
+		boolean pORq = Equacoes.second(p, q);
+		boolean pORr = Equacoes.third(p, r);
+		boolean p_OR_qANDr = Equacoes.fourth(p, qANDr);
 		boolean pORq_AND_pORr = Equacoes.fiveth(pORq, pORr);
-		
-		
+		boolean equacaoFinal = Equacoes.sixth(p_OR_qANDr, pORq_AND_pORr);
+		Funcoes.tabela(p, q, r, qANDr, pORq, pORr, p_OR_qANDr, pORq_AND_pORr, equacaoFinal);
 	}
 
 }

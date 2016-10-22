00e74a17777aedd1e2e35e4aafc06f263671055a
kant <kant@ubuntu>
2016-10-22 14:56:17 -0200

    Criação de dois Metodos

--- Raciocionio/src/Funcoes.java
+++ Raciocionio/src/Funcoes.java
@@ -37,5 +37,17 @@ public class Funcoes {
 		
 	return true;
 	}
+	
+	public static void intro(){
+		Scanner in = new Scanner (System.in);
+		System.out.println("\nEsse programa irá resolver este problema --> Pv(Q^R)<->(PvQ)^(PvR)");
+		System.out.println("Mas antes digite os valores das preposições P, Q e R.");
+		System.out.println("\n\n!!!!!!Atenção!!!!!!\n\n");
+		System.out.println("Para selecionar o valor VERDADEIRO: Digite o numero '1' e aperte [ENTER]");
+		System.out.println("Para selecionar o valor FALSO: Digite o numero '0' e aperte [ENTER]");
+		System.out.print("Aperte [ENTER] para continuar\n");
+		String start = in.nextLine();
+	}
 
 }
+
--- Raciocionio/src/Main.java
+++ Raciocionio/src/Main.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner (System.in);
 		int controler = 0;
 		boolean p = true, q = true, r = true;
 		
-		System.out.println("Esse programa irá resolver este problema");
-		
+		System.out.println("\nEsse programa irá resolver este problema --> Pv(Q^R)<->(PvQ)^(PvR)");
+		System.out.println("Mas antes digite os valores das preposições P, Q e R.");
+		System.out.println("\n\n!!!!!!Atenção!!!!!!\n\n");
+		System.out.println("Para selecionar o valor VERDADEIRO: Digite o numero '1' e aperte [ENTER]");
+		System.out.println("Para selecionar o valor FALSO: Digite o numero '0' e aperte [ENTER]");
+		System.out.print("Aperte [ENTER] para continuar\n");
+		String start = in.nextLine();
 		p = Funcoes.pegaValor(controler);
 		controler += 1;
 		q = Funcoes.pegaValor(controler);
